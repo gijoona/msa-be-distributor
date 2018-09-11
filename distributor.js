d91f13d2cbc4164abf747edae90bf8f331ecc6fa
In joon. Gong <ijgong@nglp.kr>
2018-09-11 17:49:40 +0900

    test11

--- distributor.js
+++ distributor.js
@@ -5,6 +5,7 @@ let map = {};
   date - 20180911
   distributor 클래스
   MicroService Architecture : Distributor
+  테스트1
 */
 class distributor extends require('./server.js') {  // tcpServer 클래스 상속
   constructor () {
